e35fc14cc5bd372d81e785e67354b5532f72d3b3
Lakshya Gupta <lakshya.gupta@adglobal360.com>
2025-02-05 16:13:42 +0530

    Created staffControllers

--- src/controllers/staffControllers/login.controller.js
+++ src/controllers/staffControllers/login.controller.js
@@ -1,3 +1,42 @@
-export const staffLoginController = (req, res) => {
-    console.log("Staff Login");
+import { errorResponse, successResponse } from "../../utils/apiResponse.util.js";
+import { Staff } from "../../models/staff.model.js";
+import bcryptjs from "bcryptjs"
+import { generateAccessToken } from "../../utils/generateTokens.util.js";
+
+export const staffLoginController = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password)
+            return errorResponse(res, 400, "Email and password are required");
+
+        const staff = await Staff.findOne({ email });
+        if (!staff)
+            return errorResponse(res, 404, "Staff not found");
+
+        const isPasswordCorrect = await bcryptjs.compare(password, staff.password);
+        if (!isPasswordCorrect)
+            return errorResponse(res, 401, "Invalid password");
+
+        const accessTokenData = {
+            staffId: staff._id,
+            email: staff.email,
+            role: staff.role
+        }
+
+        const staffAccessToken = generateAccessToken(accessTokenData);
+
+        const data = {
+            accessToken: staffAccessToken,
+            role: staff.role,
+            name: staff.fullName,
+            email: staff.email
+        }
+
+        successResponse(res, 400, data);
+
+    } catch (error) {
+        console.log(error);
+        return errorResponse(res, 500, res.message);
+    }
 }
--- src/controllers/staffControllers/register.controller.js
+++ src/controllers/staffControllers/register.controller.js
@@ -1,3 +1,42 @@
-export const staffRegisterController = (req, res) => {
-    console.log("Staff register");
+import { successResponse, errorResponse } from "../../utils/apiResponse.util.js";
+import { Staff } from "../../models/staff.model.js";
+import { Admin } from "../../models/admin.model.js";
+import jwt from "jsonwebtoken";
+
+export const staffRegisterController = async (req, res) => {
+    try {
+        const adminAccessToken = req.headers.accesstoken;
+        if (!adminAccessToken)
+            return errorResponse(res, 401, "No access token found");
+
+        const adminDetails = jwt.verify(adminAccessToken, process.env.ACCESS_TOKEN_SECRET);
+        const { email, role } = adminDetails;
+
+        const isAdminExist = await Admin.findOne({ email });
+        if (!isAdminExist)
+            return errorResponse(res, 404, "Admin not found");
+
+        if (role !== "Admin") {
+            return errorResponse(res, 403, "Unauthorized");
+        }
+
+        const { fullName, email: staffEmail, password } = req.body;
+        if (!fullName || !staffEmail || !password)
+            return errorResponse(res, 400, "Please fill all the fields");
+
+        const isStaffExist = await Staff.findOne({ email: staffEmail });
+        if (isStaffExist)
+            return errorResponse(res, 400, "Staff already exist with this email");
+
+        await Staff.create({
+            fullName,
+            email: staffEmail,
+            password,
+        })
+
+        successResponse(res, "Staff person created successfully",)
+    } catch (error) {
+        console.log(error);
+        return errorResponse(res, 500, "Internal Server error");
+    }
 }
