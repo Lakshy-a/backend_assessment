5bb6b07e1e35ac6c48f8171f7b6eec5af7036fa4
Lakshya Gupta <lakshya.gupta@adglobal360.com>
2025-02-05 16:14:04 +0530

    Created vendors controllers

--- src/controllers/vendorControllers/login.controller.js
+++ src/controllers/vendorControllers/login.controller.js
@@ -1,3 +1,39 @@
-export const vendorLoginController = (req, res) => {
-    console.log("Vendor Login");
+import bcryptjs from "bcryptjs"
+import { successResponse, errorResponse } from "../../utils/apiResponse.util.js"
+import { Vendor } from "../../models/vendors.model.js";
+import {generateAccessToken} from "../../utils/generateTokens.util.js"
+
+export const vendorLoginController = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password)
+            return errorResponse(res, 400, "Email and password are required");
+
+        const isExist = await Vendor.findOne({ email })
+        if (!isExist)
+            return errorResponse(res, 404, "Vendor not found");
+
+        const isPasswordCorrect = await bcryptjs.compare(password, isExist.password);
+        if (!isPasswordCorrect)
+            return errorResponse(res, 401, "Invalid password");
+
+        const accessToken = generateAccessToken({
+            id: isExist._id,
+            email: isExist.email,
+            role: isExist.role
+        });
+
+        const vendor = {
+            id: isExist._id,
+            name: isExist.fullName,
+            email: isExist.email,
+            role: isExist.role,
+            token: accessToken
+        }
+
+        successResponse(res, "Vendor logged in successfully", vendor)
+    } catch (error) {
+        return errorResponse(res, 500, error.message)
+    }
 }
--- src/controllers/vendorControllers/register.controller.js
+++ src/controllers/vendorControllers/register.controller.js
@@ -1,3 +1,26 @@
-export const vendorRegisterController = (req, res) => {
-    console.log("Vendor register");
+import { successResponse, errorResponse } from "../../utils/apiResponse.util.js"
+import { Vendor } from "../../models/vendors.model.js";
+
+export const vendorRegisterController = async (req, res) => {
+    const { fullName, email, password } = req.body;
+
+    try {
+        if (!fullName || !email || !password)
+            return errorResponse(res, 400, "Please fill all fields");
+
+        const isExist = await Vendor.findOne({ email });
+        if (isExist)
+            return errorResponse(res, 400, "Email already exist");
+
+        await Vendor.create({
+            fullName,
+            email,
+            password,
+        })
+
+        successResponse(res, "Vendor registered successfully")
+
+    } catch (error) {
+        return errorResponse(res, 500, error.message);
+    }
 }
